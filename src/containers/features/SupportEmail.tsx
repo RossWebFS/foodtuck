10358c7f68a8af79a16a3cbcefa11257f3d992de
RossWebFS <shimon.vip2006@gmail.com>
2024-05-04 23:59:24 +0300

    email sending

--- src/containers/features/SupportEmail.tsx
+++ src/containers/features/SupportEmail.tsx
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 import { InputHTMLAttributes } from "react";
 
 import { Input } from "src/components/Input";
 import { Button } from "src/components/Button";
+
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { cn } from "src/utils";
 
 interface SupportEmailProps {
   buttonStyles?: string;
@@ -17,23 +21,63 @@ export const SupportEmail = ({
   buttonContent,
   ...inputProps
 }: SupportEmailProps) => {
-  const [ emailValue, setEmailValue ] = useState<string>("");
+  const [emailValue, setEmailValue] = useState<string>("");
+  const [emailLabel, setEmailLabel] = useState<string>("Enter Your Email:");
+
+  const serviceId = "service_rcsekh3";
+  const templateId = "template_u4o3rgj";
+  const publicKey = "_zajCCUuez3mjqb2k";
+  const form = useRef(null);
+
+  const sendEmail = (e: FormEvent) => {
+    e.preventDefault();
+    const emailValidation:RegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/
+
+    if (!emailValidation.test(emailValue.toLowerCase())) {
+      setEmailLabel("Email is not correct!")
+    }
+
+    if (form.current)
+      emailjs
+        .sendForm(serviceId, templateId, form?.current, {
+          publicKey: publicKey,
+        })
+        .then(
+          () => {
+            console.log("SUCCESS!");
+          },
+          (error) => {
+            console.log("FAILED...", error.text);
+          }
+        );
+  };
 
   const onChangeEmailValue = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmailValue(e.target.value);
   };
 
   return (
-    <div>
+    <form ref={form}>
+      <label
+        className={cn("block text-sm text-gray-400 mb-1", {
+          "text-orange-400 text-md": emailLabel === "Email is not correct!"
+        })
+        }
+        htmlFor="support-email"
+      >
+        {emailLabel}
+      </label>
       <Input
+        id="support-email"
         value={emailValue}
         onChange={onChangeEmailValue}
         variant="colored"
         {...inputProps}
+        name="reply_to"
       />
-      <Button className={buttonStyles}>
+      <Button className={buttonStyles} onClick={sendEmail}>
         {buttonContent}
       </Button>
-    </div>
+    </form>
   );
 };
